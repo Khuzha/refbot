f68b66c73e149c53a7b37ebffb3b340e648cfbfd
khuzha <t.sardorkhuja@gmail.com>
2019-03-31 20:36:08 +0500

    small config.js update

--- config.js
+++ config.js
@@ -3,4 +3,6 @@ module.exports = {
     hello: 'It`s greeting text. Here is user`s referal link: t.me/RefOneBot?start=', // then index.js adds here user`s id
     invite: 'It`s text, which shows when user share link. Here is also user`s referal link: t.me/RefOneBot?start='
   }
+
+  // any other configs you need
 }
