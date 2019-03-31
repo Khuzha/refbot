c4f5a279a1246479a5699a9c2ea2b72565b7e9ec
khuzha <t.sardorkhuja@gmail.com>
2019-03-31 20:34:35 +0500

    Added config.js template

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  text: {
+    hello: 'It`s greeting text. Here is user`s referal link: t.me/RefOneBot?start=', // then index.js adds here user`s id
+    invite: 'It`s text, which shows when user share link. Here is also user`s referal link: t.me/RefOneBot?start='
+  }
+}
